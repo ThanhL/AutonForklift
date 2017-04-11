88e8c8c6ca947259373f5f60471e50ee6652e724
tHanZor <thanhl354@gmail.com>
2017-04-11 12:11:53 +1000

    Added correct steering for Forklifts

--- RollingBall/Assets/Scripts/CarControl.js
+++ RollingBall/Assets/Scripts/CarControl.js
@@ -4,6 +4,7 @@ var wheelFR : WheelCollider;
 var wheelRL : WheelCollider;
 var wheelRR : WheelCollider;
 var maxTorque : float = 50000;
+var max_steerAngle : float = 30;
 
 function Start () {
 	//rigidbody.centerOfMass.y = -0.9;
@@ -12,10 +13,10 @@ function Start () {
 
 // Calls multiple times per frame
 function FixedUpdate () {
-	wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
-	wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	wheelFR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	wheelFL.motorTorque = maxTorque * Input.GetAxis("Vertical");
 
-	wheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
-	wheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+	wheelRL.steerAngle = max_steerAngle * -Input.GetAxis("Horizontal");
+	wheelRR.steerAngle = max_steerAngle * -Input.GetAxis("Horizontal");
 	
 }
