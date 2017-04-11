e160771ee25530a7dae35dbcd45fc7db2a108b57
tHanZor <thanhl354@gmail.com>
2017-04-11 19:07:56 +1000

    TESTING WITH CAR

---
+++ RollingBall/Assets/Scripts/CarControl.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var wheelFL : WheelCollider;
+var wheelFR : WheelCollider;
+var wheelRL : WheelCollider;
+var wheelRR : WheelCollider;
+var maxTorque : float = 50000;
+
+function Start () {
+	//rigidbody.centerOfMass.y = -0.9;
+
+}
+
+// Calls multiple times per frame
+function FixedUpdate () {
+	wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+	wheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
+	wheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+	
+}
